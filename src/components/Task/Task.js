e207a072d0bddba5a2a948048e6eddb8c24c4122
asapWorker <m1904976@edu.misis.ru>
2022-08-29 09:17:35 +0300

    add task rendering

--- src/components/Task/Task.js
+++ src/components/Task/Task.js
@@ -1,13 +1,21 @@
 import React from "react";
 
 class Task extends React.Component {
-  constructor(props) {
-    super(props);
-    this.state = {};
-  }
-
   render() {
-    return <div></div>
+    const {content, importance, isFromYesterday} = this.props.taskObj;
+    const dayStatusName = (isFromYesterday) ? 'yesterday' : '';
+
+    return (
+      <div>
+        <input type="checkbox" name="task"/>
+        <span className={importance + ' ' + dayStatusName}>
+          {content}
+        </span>
+        <button className='edit-btn'>
+          edit
+        </button>
+      </div>
+    )
   }
 }
 
