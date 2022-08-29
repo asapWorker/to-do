781bc97a6b2db700d2fac16e15bfbe37e9441914
asapWorker <m1904976@edu.misis.ru>
2022-08-29 09:13:14 +0300

    add tasks' list rendering

--- src/components/List/List.js
+++ src/components/List/List.js
@@ -1,11 +1,23 @@
 import React from "react";
+import Task from "../Task/Task";
 
 import Task from "../Task/Task";
 
 class List extends React.Component {
   render() {
+    let tasks = 'No tasks';
+    if (this.props.list) {
+      tasks = this.props.list.map((task) => {
+        return (
+          <Task
+            key={task.key}
+            taskObj={task.obj}
+          />
+        )
+      })
+    }
     return <div>
-
+      {tasks}
     </div>
   }
 }
