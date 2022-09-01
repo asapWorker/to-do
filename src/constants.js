c48d6bcebd20e81aca10fedd831f93e8cbbe2f27
asapWorker <m1904976@edu.misis.ru>
2022-09-01 10:33:40 +0300

    add constants.js with constants

---
+++ src/constants.js
@@ -0,0 +1,15 @@
+//days constants
+//App.js
+export const FIRST_DAY = 0;
+export const LAST_DAY = 6;
+export const DAYS_C = 7;
+
+//task importance options
+//App.js, Editor.js
+export const IMPORTANCE_OPTIONS = ["less", "middle", "very"];
+export const IMPORTANCE = {
+  //matching option's name and id
+  less: 0,
+  middle: 1,
+  very: 2,
+}
